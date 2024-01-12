eb19d87a480c79396ac70d0eb5f47d326269f50e
furiax <carlmalfliet@proximus.be>
2024-01-12 14:07:10 +0100

    add heroes and message services

--- TourOfHeroes.Furiax/tourofheroes.furiax.client/src/app/heroes/heroes.component.ts
+++ TourOfHeroes.Furiax/tourofheroes.furiax.client/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,21 @@ import { HEROES } from '../mock-heroes';
 
 })
 export class HeroesComponent {
-  heroes = HEROES;
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService : MessageService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
 
