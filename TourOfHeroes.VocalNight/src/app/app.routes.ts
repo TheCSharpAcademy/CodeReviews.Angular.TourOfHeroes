6f74813fe838f8ba65a3dd8fc056d71e8feea810
Vocal <vocalnight@protonmail.com>
2024-03-03 20:41:24 -0300

    project completed

---
+++ TourOfHeroes.VocalNight/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+export const routes: Routes = [
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'detail/:id', component: HeroDetailComponent },
+];
---
+++ TourOfHeroes.VocalNight/src/app/messages/messages.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [NgIf, NgFor],
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css'
+})
+export class MessagesComponent {
+
+  constructor(public messageService: MessageService) {}
+
+}
